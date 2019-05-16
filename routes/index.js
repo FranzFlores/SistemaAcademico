fa72624a5b319e6da92b11007f2e132ccf2590a6
Franz Flores <andresfloresgallardo@gmail.com>
2019-05-16 15:28:48 -0500

    Vista de la pagina de Inicio
    Vista de la pagina de Inicio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//Menu de Registro 
+router.get('/menu',function(req,res,next){
+  console.log(req);
+  //res.render('register/menu-register',{title: 'Academy Loja'});
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Academy Loja' });
 });
+
 
 module.exports = router;  
