b1c8011f730676b922082d744806b85df7f467a3
Franz Floes <andresfloresgallardo@unl.edu.ec>
2019-06-06 12:26:22 +1400

    Metodo Eliminar Facultad

--- models/faculty.model.js
+++ models/faculty.model.js
@@ -4,8 +4,7 @@ const { Schema } = mongoose;
 
 const facultySchema = new Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true },
-    status: {type:Boolean,required:true, default: true}
+    description: { type: String, required: true }
 }, { timestamps: { createdAt: 'created_at', updateAt: 'update_at' } });
 
 module.exports = mongoose.model('Faculty', facultySchema);
